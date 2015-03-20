9d2db4248d91050b84d6d357332914990358653a
yoonji <yoonji2013@gmail.com>
2015-03-19 20:01:36 -0400

    working carousel functionality

---
+++ README.md
@@ -0,0 +1,3 @@
+# Custom jQuery Carousel
+
+Allows user to slide images, text or videos in and out of view by clicking arrows.
---
+++ js/carousel.js
@@ -0,0 +1,32 @@
+$(function(){
+
+  var numberSlides = $('.slide').size();
+  var slideWidth = $('.slide').width();
+  var scrollValue = 0;
+
+  // set the width #tray
+  $('#tray').width(numberSlides * slideWidth);
+
+  $('#next').click(function(event){
+    event.preventDefault();
+    if (scrollValue < numberSlides - 1) {
+      scrollValue += 1;
+    } else {
+      scrollValue = 0;
+    }
+    var position = scrollValue * slideWidth;
+    $('#tray').animate({right: position}, 400);
+  });
+
+  $('#prev').click(function(event){
+    event.preventDefault();
+    if (scrollValue > 0) {
+      scrollValue -= 1;
+    } else {
+      scrollValue = numberSlides - 1;
+    }
+    var position = scrollValue * slideWidth;
+    $('#tray').animate({right: position}, 400);
+  });
+
+});
